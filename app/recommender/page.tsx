e685757cf9c02c787d1c5ef80f8bf922e4dea7bf
FindMalek <malekgarahellalbus@gmail.com>
2024-02-22 00:07:17 +0100

    :heavy_plus_sign: Added: Taxonomy Boilerplate.

---
+++ app/recommender/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Recommender",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Recommender</h1>
+    </div>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Chatmater",
+  description:
+    "Designed to automate and personalize messaging, this web application empowers creators to maintain consistent, meaningful connections with their fans without the manual overhead.",
+  url: "https://chatmater.vercel.app",
+  ogImage: "https://chatmater.vercel.app/og.svg",
+  links: {
+    twitter: "https://twitter.com/foundmalek",
+    github: "https://github.com/FindMalek/Chatmater",
+  },
+};
